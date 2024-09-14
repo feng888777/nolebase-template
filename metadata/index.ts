844da4091603d74a15f6be33f444abc62231f29a
feng888777 <161155339+feng888777@users.noreply.github.com>
2024-09-14 23:01:19 +0800

    Update index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,3 +1,5 @@
+
+
 /** 文本 */
 export const siteName = '彩极术'
 export const siteShortName = '彩极术'
@@ -6,7 +8,13 @@ export const siteDescription = '极致实用彩票技术知识库'
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
 
+/** Repo */
+export const githubRepoLink = ''
+/** Discord */
+export const discordLink = ''
+
 /** 无协议前缀域名 */
 export const plainTargetDomain = 'caijishu.us.kg/'
 /** 完整域名 */
 export const targetDomain = `https://${plainTargetDomain}`
+
