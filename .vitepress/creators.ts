4ac3c250abd049ba4395b9ec07723372abe8938a
feng888777 <161155339+feng888777@users.noreply.github.com>
2024-09-14 22:56:45 +0800

    Update creators.ts

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -1,1 +1,55 @@
 
+export interface SocialEntry {
+  type: 'github' | 'twitter' | 'email'
+  icon: string
+  link: string
+}
+
+export interface Creator {
+  avatar: string
+  name: string
+  username?: string
+  title?: string
+  org?: string
+  desc?: string
+  links?: SocialEntry[]
+  nameAliases?: string[]
+  emailAliases?: string[]
+}
+
+const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
+
+export const creators: Creator[] = [
+  {
+    name: '',
+    avatar: '',
+    username: '',
+    title: ' ',
+    desc: '',
+    links: [
+      { type: 'github', icon: 'github', link: '' },
+      { type: 'twitter', icon: 'twitter', link: '' },
+    ],
+    nameAliases: ['nekomeowww', '', '', ' ', ' '],
+    emailAliases: [''],
+  },
+  {
+    name: '',
+    avatar: '',
+    username: '',
+    title: '',
+    desc: '',
+    links: [
+      { type: 'github', icon: 'github', link: '' },
+      { type: 'twitter', icon: 'twitter', link: '' },
+    ],
+    nameAliases: ['LittleSound', '', '', ' ', ' ', ' ', ''],
+    emailAliases: ['', ''],
+  },
+].map<Creator>((c) => {
+  c.avatar = c.avatar || getAvatarUrl(c.username)
+  return c as Creator
+})
+
+export const creatorNames = creators.map(c => c.name)
+export const creatorUsernames = creators.map(c => c.username || '')
